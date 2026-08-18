a6ce13babecd5b6a7fd4ced9fded5d1eb71d395e
salma570 <salmakhalawy@gmail.com>
2026-08-18 23:09:48 +0300

    new

---
+++ backend/Context/Migrations/20260818200004_AddAnnotatorSoftDelete.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnnotatorSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "Annotators",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Annotators",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Annotators_IsDeleted",
+                table: "Annotators",
+                column: "IsDeleted");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Annotators_IsDeleted",
+                table: "Annotators");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Annotators");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Annotators");
+        }
+    }
+}
